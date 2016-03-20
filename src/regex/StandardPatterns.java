b9d6110650a16982cc9b14afaa003aa80b8df347
Yitzi <yitzchakhorowitz@gmail.com>
2016-03-20 00:40:39 -0400

    initial commit

---
+++ src/regex/StandardPatterns.java
@@ -0,0 +1,26 @@
+package regex;
+
+/**
+ * Created by Yitzi on 3/19/2016.
+ */
+public class StandardPatterns {
+
+//    public static RegEx USPhoneNumber() {
+//
+//    }
+
+//    public static RegEx emailAddress() {
+//        RegEx regex = RegEx.zeroOrMore(RegEx.alphaNumeric())
+//                .then(RegEx.zeroOrOne(RegEx.literal("."))
+//                .then(RegEx.oneOrMore(RegEx.alphaNumeric()))
+//                .then(RegEx.exactlyXNumberOfTimes(1, RegEx.literal("@"))
+//                .then(RegEx.oneOrMore(RegEx.alphaNumeric()))
+//                .then(RegEx.exactlyXNumberOfTimes(1, RegEx.literal(".")))
+//                .then(RegEx.oneOrMore(RegEx.alphaNumeric()))))
+//                .then(RegEx.zeroOrOne(RegEx.literal(".")))
+//                .then(RegEx.rangeOfTimes(0, 4, RegEx.alphaNumeric()))
+//                ;
+//
+//        return regex;
+//    }
+}
